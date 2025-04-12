806fddab8d1e05f3d2dcdd410bdd95e54ea2a9e5
yjunechoe <jchoe001@gmail.com>
2025-04-12 16:11:44 -0400

    some fixes

--- app.R
+++ app.R
@@ -7,6 +7,9 @@ library(listviewer)
 library(ggplot2)
 library(DT)
 library(ggtrace)
+check_installed("ggtrace (>= 0.7.4)")
+
+source("utils.R")
 
 # UI layout
 ui <- page_sidebar(
@@ -275,87 +278,6 @@ server <- function(input, output, session) {
   })
 
   observeEvent(input$debug_btn, { browser() })
-}
-
-initial_code <- r"(p <- ggplot(penguins, aes(x = species)) +
-  geom_bar(
-    aes(
-      fill = species,
-      color = after_scale(alpha(fill, .5))
-    ),
-    linewidth = 3
-  ) +
-  geom_label(
-    aes(
-      y = after_stat(count),
-      label = after_stat(sprintf("%.1f%%", count/sum(count) * 100))
-    ),
-    size = 5,
-    position = position_nudge(y = 10),
-    stat = "count"
-  )
-)"
-
-# Simplified function definitions
-fns <- list(
-  `Layer pipeline` = c(
-    "Layer$layer_data",
-    "Layer$setup_layer",
-    "Layer$compute_aesthetics",
-    "Layer$compute_statistic",
-    "Layer$map_statistic",
-    "Layer$compute_geom_1",
-    "Layer$compute_position",
-    "Layer$compute_geom_2",
-    "Layer$finish_statistics",
-    "Layer$draw_geom"
-  )
-  # `Built layer information` = c("layer_data", "layer_grob")
-)
-
-fn_to_expr <- function(fn) {
-  parsed <- parse_expr(fn)
-  # Built layer information
-  if (is.symbol(parsed)) {
-    if (grepl(x = fn, "^layer_")) {
-      # ggplot2
-      expr <- call2(fn, p = sym("p"), i = sym("i"))
-    } else {
-      # ggtrace
-      fn <- paste0("layer_", fn)
-      expr <- call2(fn, p = sym("p"), i = sym("i"), .ns = "ggtrace")
-    }
-  } else {
-    # ggproto
-    if (!strsplit(fn, "\\$")[[1]][1] %in% getNamespaceExports("ggplot2")) {
-      fn <- paste0("ggplot2:::", fn)
-    }
-    expr <- call2(
-      "inspect_return",
-      x = sym("p"),
-      method = parse_expr(fn),
-      cond = call2("layer_is", sym("i"))
-    )
-  }
-  expr_deparse(expr)
-}
-
-local_call <- function(expr) {
-  exprs <- if (is.character(expr)) {
-    parse_exprs(expr)
-  } else {
-    if (rlang::is_bare_list(expr)) {
-      expr
-    } else {
-      list(expr)
-    }
-  }
-  call2("local", call2("{", !!!exprs))
-}
-
-poorman_styler <- function(expr) {
-  gsub(x = expr, ",", ",\n ") |>
-    gsub(x = _, "^(\\w+\\()(.*)(\\))$", "\\1\n  \\2\n\\3\n")
 }
 
 shinyApp(ui, server, options = list(launch.browser = TRUE))
